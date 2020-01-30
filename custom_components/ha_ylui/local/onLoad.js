9ae370561aa0722dae082f7e5de3fa4002346af9
zhulu <shaonianzhentan@sina.cn>
2020-01-30 15:50:28 +0800

    添加MDI图标

--- custom_components/ha_ylui/local/onLoad.js
+++ custom_components/ha_ylui/local/onLoad.js
@@ -6,52 +6,39 @@
  */
 
 YL.onLoad(function () {
-  // 读取url中load参数，如localhost/ylui/index.html?load=basic
-  var load = Yuri2.parseURL().params.load;
-  var file;
-  // 当load === 'ylui-storage'时，尝试加载浏览器缓存
-  if (load === YL.static.localStorageName && localStorage.getItem(YL.static.localStorageName)) {
-    console.log('loading')
-    YL.init();
-    return;
-  } else if (load === YL.static.localStorageName) {
-    file = 'basic';
-  }
-  console.log('初始化配置数据')
-    ; (() => {
-      // 设置全屏模式
-      try {
-        let haPanelIframe = top.document.body
-          .querySelector("home-assistant")
-          .shadowRoot.querySelector("home-assistant-main")
-          .shadowRoot.querySelector("app-drawer-layout partial-panel-resolver ha-panel-iframe").shadowRoot
-        let ha_card = haPanelIframe.querySelector("iframe");
-        ha_card.style.position = 'absolute'
-        haPanelIframe.querySelector('app-toolbar').style.display = 'none'
-        ha_card.style.top = '0'
-        ha_card.style.height = '100%'
-      } catch (ex) {
+  // 设置全屏模式
+  try {
+    let haPanelIframe = top.document.body
+      .querySelector("home-assistant")
+      .shadowRoot.querySelector("home-assistant-main")
+      .shadowRoot.querySelector("app-drawer-layout partial-panel-resolver ha-panel-iframe").shadowRoot
+    let ha_card = haPanelIframe.querySelector("iframe");
+    ha_card.style.position = 'absolute'
+    haPanelIframe.querySelector('app-toolbar').style.display = 'none'
+    ha_card.style.top = '0'
+    ha_card.style.height = '100%'
+  } catch (ex) {
 
+  }
+  // 监听数据变化
+  top.window.addEventListener('storage', function (e) {
+    // console.log(e);
+    if (e.key === 'ylui-storage') {
+      if (e.oldValue != e.newValue) {
+        // 更新
+        this.fetch(`${top.location.pathname}-api`, {
+          method: 'post',
+          body: JSON.stringify({
+            type: 'set',
+            data: e.newValue
+          })
+        }).then(res => res.json()).then(res => {
+          console.log(res)
+        })
       }
-      // 监听数据变化
-      top.window.addEventListener('storage', function (e) {
-        // console.log(e);
-        if (e.key === 'ylui-storage') {
-          if (e.oldValue != e.newValue) {
-            // 更新
-            this.fetch(`${top.location.pathname}-api`, {
-              method: 'post',
-              body: JSON.stringify({
-                type: 'set',
-                data: e.newValue
-              })
-            }).then(res => res.json()).then(res => {
-              console.log(res)
-            })
-          }
-        }
-      });
-    })();
+    }
+  });
+  // 加载数据
   fetch(`${top.location.pathname}-api`, {
     method: 'post',
     body: JSON.stringify({
@@ -62,18 +49,17 @@ YL.onLoad(function () {
       let obj = JSON.parse(res.data)
       YL.init(obj)
     } else {
-      // 从json文件读取
-      file = file || load || 'basic';
-      var save = /^\w+$/.test(file) ? './saves/' + file + '.json' : file;
-      Yuri2.loadContentFromUrl(save, 'GET', function (err, text) {
-        if (!err) {
-          var data = JSON.parse(text);
-          YL.init(data);
-        } else {
-          alert('YLUI读取配置错误，初始化失败');
-        }
-      });
+      throw new Error('没有配置文件')
     }
+  }).catch(ex => {
+    Yuri2.loadContentFromUrl(`./saves/basic.json`, 'GET', function (err, text) {
+      if (!err) {
+        var data = JSON.parse(text);
+        YL.init(data);
+      } else {
+        alert('YLUI读取配置错误，初始化失败');
+      }
+    });
   })
 
 });
